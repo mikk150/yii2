00e95ca122ea2162a9b5bed90f274e0d9cadae8a
Mikk Tendermann <mikk150@gmail.com>
2018-01-10 17:39:47 +0200

    Add postgres tests as well

--- tests/framework/db/pgsql/ColumnSchemaBuilderTest.php
+++ tests/framework/db/pgsql/ColumnSchemaBuilderTest.php
@@ -8,6 +8,8 @@
 namespace yiiunit\framework\db\pgsql;
 
 use yii\db\ColumnSchemaBuilder;
+use yii\db\Expression;
+use yii\db\Schema;
 
 /**
  * ColumnSchemaBuilderTest tests ColumnSchemaBuilder for Oracle.
@@ -26,5 +28,30 @@ class ColumnSchemaBuilderTest extends \yiiunit\framework\db\ColumnSchemaBuilderT
     public function getColumnSchemaBuilder($type, $length = null)
     {
         return new ColumnSchemaBuilder($type, $length, $this->getConnection());
+    }
+
+    /**
+     * @return array
+     */
+    public function typesProvider()
+    {
+        return [
+            ['integer NULL DEFAULT NULL', Schema::TYPE_INTEGER, null, [
+                ['unsigned'], ['null'],
+            ]],
+            ['integer(10)', Schema::TYPE_INTEGER, 10, [
+                ['unsigned'],
+            ]],
+            ['timestamp() WITH TIME ZONE NOT NULL', 'timestamp() WITH TIME ZONE', null, [
+                ['notNull'],
+            ]],
+            ['timestamp() WITH TIME ZONE DEFAULT NOW()', 'timestamp() WITH TIME ZONE', null, [
+                ['defaultValue', new Expression('NOW()')],
+            ]],
+            ['integer(10)', Schema::TYPE_INTEGER, 10, [
+                ['comment', 'test'],
+            ]],
+            ['json', Schema::TYPE_JSON, null, []],
+        ];
     }
 }
